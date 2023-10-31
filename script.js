4924209e9d78cdea59a57d48252eb943a559a1b7
AustineBill <auztine.malic@gmail.com>
2023-10-31 09:32:11 +0800

    Updated Hamburger

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ function toggleContent(button) {
 }
 
 
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+
+
 
 
 
